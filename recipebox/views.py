ab9e2a5e29c9218e997e75dfa01b62ae0860e66d
nate <nate998877@gmail.com>
2019-10-29 16:42:46 -0400

    changed views, models, and templates to meet the requirements of assessment

--- recipebox/views.py
+++ recipebox/views.py
@@ -5,8 +5,9 @@ from recipebox.models import Recipe, Author
 
 def index(request):
     html = "index.html"
+    recipe_list = Recipe.objects.all()
 
-    return render(request, html)
+    return render(request, html, {"recipes": recipe_list})
 
 
 def recipe(request, key_id):
@@ -18,20 +19,30 @@ def recipe(request, key_id):
 
 def author(request, key_id):
     author_obj = Author.objects.get(pk=key_id)
+    recipes = [x for x in Recipe.objects.filter(author=author_obj)]
     author = "author.html"
 
-    return render(request, author, {"author": author_obj})
+    return render(request, author, {"author": author_obj, "recipes": recipes})
 
 
 def recipes(request):
     recipes_list = [x for x in Recipe.objects.all()]
-    books = "recipes.html"
+    recipes = "recipes.html"
 
-    return render(request, books, {"recipes": recipes_list})
+    return render(request, recipes, {"recipes": recipes_list})
 
 
 def authors(request):
     authors_list = [x for x in Author.objects.all()]
+    auth_arr = []
+    for auth in authors_list:
+        obj = {}
+        obj["author"] = auth
+        recipes = Recipe.objects.filter(author=auth)
+        obj["recipes"] = recipes
+        auth_arr.append(obj)
+
+
     authors = "authors.html"
 
-    return render(request, authors, {"authors": authors_list})
+    return render(request, authors, {"authors": auth_arr})
